afba8f85b3388b6af348906cf6d39e5b795b86e7
alinakassym <alinakassym@gmail.com>
2025-07-29 12:44:30 +0500

    feat(router): add CatalogPage layout with product list and nested routing via Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
 import styles from "./App.module.css";
 
+const fetchProductsList = [
+  {
+    id: 1,
+    name: "4K Smart TV Samsung 55",
+    price: 749,
+  },
+  {
+    id: 2,
+    name: "iPhone 16 Pro Max 1024GB",
+    price: 999,
+  },
+  {
+    id: 3,
+    name: "Dyson Supersonic Hair Dryer",
+    price: 429,
+  },
+];
 const MainPage = () => <div className="main-page">Main page</div>;
-const CatalogPage = () => <div className="catalog-page">Catalog page</div>;
+const CatalogPage = () => (
+  <div className="catalog-page">
+    <h3>Catalog</h3>
+    <ul>
+      {fetchProductsList.map(({ id, name }) => (
+        <li key={id}>
+          <Link to="product">{name}</Link>
+        </li>
+      ))}
+    </ul>
+    <Outlet />
+  </div>
+);
 const ContactsPage = () => <div className="contacts-page">Contacts page</div>;
+const ProductPage = () => <div className="product-page">Product page</div>;
 
 function App() {
   return (
@@ -24,7 +54,9 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/catalog" element={<CatalogPage />}>
+          <Route path="product" element={<ProductPage />} />
+        </Route>
         <Route path="/contacts" element={<ContactsPage />} />
       </Routes>
     </div>
