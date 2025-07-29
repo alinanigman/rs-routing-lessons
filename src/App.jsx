1b6e38bcc871c2f044fb2d8c52879230c602cff2
alinakassym <alinakassym@gmail.com>
2025-07-29 12:29:54 +0500

    feat: create menu with main, catalog, contacts pages using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { Routes, Route, Link } from "react-router-dom";
 import styles from "./App.module.css";
 
+const MainPage = () => <div className="main-page">Main page</div>;
+const CatalogPage = () => <div className="catalog-page">Catalog page</div>;
+const ContactsPage = () => <div className="contacts-page">Contacts page</div>;
+
 function App() {
-  return <div className={styles.App}></div>;
+  return (
+    <div className={styles.App}>
+      <div>
+        <h3>Menu</h3>
+        <ul>
+          <li>
+            <Link to="/">Main</Link>
+          </li>
+          <li>
+            <Link to="/catalog">Catalog</Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contacts</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
